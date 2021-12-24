d89dcffe2ba3bddc46f1a938d386d822e4e18c5d
Diego Granados <diego@codeando.edu.pe>
2021-12-24 11:31:02 -0400

    Proyecto Terminado

--- main.js
+++ main.js
@@ -5,3 +5,39 @@ userName ? console.log(`Hello, ${userName}!`) : console.log ('Hello!');
 const userQuestion = 'Will i be millonare some day?';
 
 userName ? console.log(`${userName} asked: ${userQuestion}`) : console.log (`The user asked: ${userQuestion}`);
+
+const randomNumber =  Math.floor(Math.random()*8);
+
+let eightBall = '';
+
+switch (randomNumber) {
+  case 1 :
+  eightBall = 'It is certain';
+  break;
+  case 2 :
+  eightBall = 'It is decidedly so';
+  break;
+  case 3 :
+  eightBall = 'Reply hazy try again';
+  break;
+  case 4 :
+  eightBall = 'Cannot predict now';
+  break;
+  case 5 :
+  eightBall = 'Do not count on it';
+  break;
+  case 6 :
+  eightBall = 'My sources say no';
+  break;
+  case 7 :
+  eightBall = 'Outlook not so good';
+  break;
+  case 8 :
+  eightBall = 'Signs point to yes';
+  break;
+  case 0 :
+  eightBall = 'Yes';
+  break;
+}
+
+console.log(`The eight ball answered: ${eightBall}`);
