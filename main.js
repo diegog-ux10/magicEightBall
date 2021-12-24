8303dbf72dc913d359aa40f1de6d6f8ceb8cd5dd
Diego Granados <diego@codeando.edu.pe>
2021-12-24 11:09:18 -0400

    primer commit

---
+++ main.js
@@ -0,0 +1,7 @@
+let userName= '';
+
+userName ? console.log(`Hello, ${userName}!`) : console.log ('Hello!');
+
+const userQuestion = 'Will i be millonare some day?';
+
+userName ? console.log(`${userName} asked: ${userQuestion}`) : console.log (`The user asked: ${userQuestion}`);
